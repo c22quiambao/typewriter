de673a67ce49e4ce025b05586e6dbba672a2fd80
c22quiambao <christelquiambao@gmail.com>
2023-11-22 05:18:10 -0800

    code clean up

--- typewriter.js
+++ typewriter.js
@@ -2,12 +2,12 @@ const sentence = "hello there from lighthouse labs";
 
 let counter = 1000;
 
-for (let index in sentence){
+for (let index in sentence) {
   setTimeout(() => {
     process.stdout.write(sentence[index]);
-    if (index >= sentence.length -1){
+    if (index >= sentence.length - 1) {
       console.log('\n');
     }
-  }, counter)
+  }, counter);
   counter += 50;
-};
+}
